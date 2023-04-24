a5c3636c7fa6b142414df289fe88e8575387d3d9
jhlee0667 <jhlee0667@gmail.com>
2023-04-24 15:42:14 +0900

    update 20230424

--- src/CUDA/compile.m
+++ src/CUDA/compile.m
@@ -1,1 +1,1 @@
-mexcuda -R2018a -output mset_cuda_kernel_t3 mex_main.cu mex_tools.cpp mset.cu utility.cu -L~/opt/local/cuda/cuda_11.3/lib64/ -lcufft
+mexcuda -R2018a -output mset_cuda_kernel mex_main.cu mex_tools.cpp mset.cu utility.cu -L~/opt/local/cuda/cuda_11.3/lib64/ -lcufft
